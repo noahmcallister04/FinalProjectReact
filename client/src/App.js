3775eed7d523de2049da41fbbcb81c3b6364133c
Colin Buchheit <130728626+ColinBuchheit@users.noreply.github.com>
2024-05-04 21:54:25 -0500

    React

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route
+}
+function App() {
+  return (
+    <div className="App">
+    <BrowserRouter>
+          
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
